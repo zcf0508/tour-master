15fb6aa83730803d32af3b7d89e677070cf32af8
zcf0508 <zcf0508@live.com>
2025-02-08 19:46:03 +0800

    feat: showStep function

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,43 +1,37 @@
-import { createOverlaySvg } from '../../src/core/renderer/overlay';
-import { showPopover } from '../../src/core/renderer/popover';
+import { showStep } from '../../src/core/renderer';
 
-const referenceEl = document.getElementById('step_1');
+const referenceEl = document.getElementById('step_1')!;
 
-const referenceElRect = referenceEl!.getBoundingClientRect();
+referenceEl.addEventListener('click', () => {
+  showStep(
+    referenceEl,
+    () => {
+      const tooltipEl = document.createElement('div') as HTMLElement;
+      tooltipEl.textContent = 'My tooltip with more content';
 
-const overlaySvg = createOverlaySvg(
-  [
-    {
-      x: referenceElRect.left,
-      y: referenceElRect.top,
-      width: referenceElRect.width,
-      height: referenceElRect.height,
+      Object.assign(tooltipEl.style, {
+        'position': 'absolute',
+        'background': '#fff',
+        'z-index': 10001,
+        'padding': '4px',
+        'border-radius': '4px',
+      });
+
+      document.body.appendChild(tooltipEl);
+
+      return tooltipEl;
     },
-  ],
-  {
-    stagePadding: 4,
-    stageRadius: 4,
-  },
-);
+    () => {
+      const referenceElRect = referenceEl.getBoundingClientRect();
 
-document.body.appendChild(overlaySvg);
-
-showPopover(
-  referenceEl!,
-  () => {
-    const tooltipEl = document.createElement('div') as HTMLElement;
-    tooltipEl.textContent = 'My tooltip with more content';
-
-    Object.assign(tooltipEl.style, {
-      'position': 'absolute',
-      'background': '#fff',
-      'z-index': 10001,
-      'padding': '4px',
-      'border-radius': '4px',
-    });
-
-    document.body.appendChild(tooltipEl);
-
-    return tooltipEl;
-  },
-);
+      return [
+        {
+          x: referenceElRect.left,
+          y: referenceElRect.top,
+          width: referenceElRect.width,
+          height: referenceElRect.height,
+        },
+      ];
+    },
+  );
+});
--- src/core/renderer/index.ts
+++ src/core/renderer/index.ts
@@ -1,1 +1,26 @@
-export const a = 1;
+import type { MaybeRef } from '@vue/reactivity';
+import type { StageDefinition } from './overlay';
+import { toValue } from '@vue/reactivity';
+import { createOverlaySvg } from './overlay';
+import { showPopover } from './popover';
+
+export function showStep(
+  referenceEl: MaybeRef<HTMLElement>,
+  createTooltipEl: () => MaybeRef<HTMLElement>,
+  stages: StageDefinition[] | (() => StageDefinition[]),
+): void {
+  const overlaySvg = createOverlaySvg(
+    toValue(stages),
+    {
+      stagePadding: 4,
+      stageRadius: 4,
+    },
+  );
+
+  document.body.appendChild(overlaySvg);
+
+  showPopover(
+    referenceEl,
+    createTooltipEl,
+  );
+}
