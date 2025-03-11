28a54a29eedc8d07c09244ebbf3643a996d71e75
zcf0508 <zcf0508@live.com>
2025-03-11 17:52:21 +0800

    fix(lint): same property

--- build.config.ts
+++ build.config.ts
@@ -4,7 +4,6 @@ export default defineBuildConfig({
   entries: [
     'src/index',
   ],
-  declaration: true,
   clean: true,
   declaration: 'node16',
   externals: ['@vue/reactivity', '@floating-ui/dom', '@vueuse/shared'],
